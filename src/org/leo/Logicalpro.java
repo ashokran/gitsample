d07b8be632ab533fc90f6b99900bd80b1790b411
ashokran <ashokranjan777@gmail.com>
2023-10-04 21:36:25 +0530

    first commit

---
+++ src/org/leo/Logicalpro.java
@@ -0,0 +1,29 @@
+package org.leo;
+
+public class Logicalpro {
+
+	public static void main(String[] args) {
+		int d=7854587;
+		int j=0;
+		int temp=d;
+		int count=0;
+		int sum=0;
+		while(d>0) {
+		   int a=d/10;
+		   int b=d%10;
+		   j=b+(j*10);
+		   count++;
+		   sum=sum+b;
+		   d=a;
+		}
+      System.out.println(j);
+      if (temp==j) {
+    	  System.out.println("Palindrome Number");
+	} else {
+         System.out.println("Not Palindrome Number");
+	}
+      System.out.println("Number of Digits;"+count);
+      System.out.println("Sum of Numbers:"+sum);
+	}
+    
+}
